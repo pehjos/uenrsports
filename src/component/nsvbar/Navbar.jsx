0511f80b236a0b2254063c7280e4bf3be1983862
Pehjos <71464455+pehjos@users.noreply.github.com>
2022-10-06 08:38:51 +0000

    Update Navbar.jsx

--- src/component/nsvbar/Navbar.jsx
+++ src/component/nsvbar/Navbar.jsx
@@ -7,7 +7,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import './Navbar.css'
 import {Search} from 'react-bootstrap-icons'
 import { TranslatorProvider, useTranslate } from "react-translate"
-
+import {Link} from 'react-router-dom'
 let translations = {
   locale: "en",
   Home: {
@@ -27,54 +27,84 @@ function Navbarcomponent() {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             {/* DROP DOWN 1 */}
-            <Nav.Link href="/">HOME</Nav.Link>
+            <Nav.Link >
+           <Link className='link' to="/">HOME</Link>
+           </Nav.Link >
                {/* DROP DOWN 1 */}
             <NavDropdown title="SERVICES" id="basic-nav-dropdown">
 
-              <NavDropdown.Item  href="/article">
-        WRITE ARTICLE
+              <NavDropdown.Item  >
+              <Link className='link' to="/article">WRITE ARTICLE</Link>
               </NavDropdown.Item>
               
-              <NavDropdown.Item href="rent">RENT SCHOOL FIELD</NavDropdown.Item>
+              <NavDropdown.Item href="rent">
+              
+                <Link className='link' to="/rent">  RENT SCHOOL FIELD</Link>   
+                </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="NEWS" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/ball">FOOTBALL</NavDropdown.Item>
-              <NavDropdown.Item href="/volley">
- VOLLEYBALL
+              <NavDropdown.Item >
+           
+              <Link className='link' to="/ball">  FOOTBALL</Link>   
+                </NavDropdown.Item>
+              <NavDropdown.Item >
+              <Link className='link' to="/volley">VOLLEYBALL</Link>  
               </NavDropdown.Item>
               
-              <NavDropdown.Item href="basket">BASKETBALL</NavDropdown.Item>
+              <NavDropdown.Item >
+              <Link className='link' to="/basket">BASKETBALL</Link>  
+                </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="MATCH" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/schedule">SCHEDULE</NavDropdown.Item>
-              <NavDropdown.Item href="/highlights">
-HIGHLIGHTS
+              <NavDropdown.Item >
+              <Link className='link' to="/schedule">SCHEDULE</Link>  
+                </NavDropdown.Item>
+              <NavDropdown.Item>
+              <Link className='link' to="/highlights">HIGHLIGHTS</Link>  
               </NavDropdown.Item>
               
-              <NavDropdown.Item href="/news">NEWS</NavDropdown.Item>
+              <NavDropdown.Item >
+              <Link className='link' to="/news">NEWS</Link>  
+                </NavDropdown.Item>
   
             </NavDropdown>
             <NavDropdown title="PLAYERS" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/football">FOOTBALL</NavDropdown.Item>
-              <NavDropdown.Item href="/basketball">
-BASKETBALL
+              <NavDropdown.Item >
+              <Link className='link' to="/football">FOOTBALL</Link>  
+                </NavDropdown.Item>
+              <NavDropdown.Item >
+              <Link className='link' to="/basketball">BASKETBALL</Link>  
               </NavDropdown.Item>
               
-              <NavDropdown.Item href="/volleyvall">VOLLEYBALL</NavDropdown.Item>
+              <NavDropdown.Item >
+              <Link className='link' to="/volleyball">VOLLEYBALL</Link>  
+                </NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="HIGHLIGHTS" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/highlights">ALL SPORTS</NavDropdown.Item>
+              <NavDropdown.Item >
+                
+              <Link className='link' to="/highlights">ALL SPORTS</Link>  
+                </NavDropdown.Item>
              
             </NavDropdown>
-            <Nav.Link href="/contact">CONTACT US</Nav.Link>
+            <Nav.Link >
+            <Link className='link' to="/contact">CONTACT US</Link>  
+              
+              </Nav.Link>
            
    
-            <Nav.Link href="/about">ABOUT US</Nav.Link>
-         {!user?(<Nav.Link href="/Login">LOGIN</Nav.Link>):(<Nav.Link href="/home">{user?.result.name}</Nav.Link>)} 
+            <Nav.Link >  <Link className='link' to="/about">ABOUT US</Link>  </Nav.Link>
+         {!user?(<Nav.Link >  <Link className='link' to="/schedule">LOGIN</Link>  </Nav.Link>):(<Nav.Link >
+
+           <Link className='link' to="/home"> {user?.result.name}</Link>  
+           </Nav.Link>)} 
           </Nav>
         </Navbar.Collapse>
   
-        <Nav.Link href="/search"> <Search/></Nav.Link>
+        <Nav.Link href="/search"> 
+        <Link className='link' to="/search"><Search/></Link>  
+
+        </Nav.Link>
     
       </Container>
     </Navbar>
